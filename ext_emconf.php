5c6a422b4edc84558b5f91ec6a9e95db04a8eefb
Georg Ringer <georg.ringer@gmail.com>
2017-02-25 21:04:22 +0100

    [TASK] Fix ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,45 +1,20 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "a21glossary".
-#
-# Auto generated 17-02-2012 15:52
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Autoswitch to list view',
-	'description' => '',
-	'category' => 'fe',
-	'shy' => 0,
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => 0,
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'lockType' => '',
-	'author' => 'Georg Ringer',
-	'author_email' => 'typo3@ringerge.org',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'version' => '1.0.0',
-	'_md5_values_when_last_written' => '',
-	'constraints' => array(
-		'depends' => array(),
-		'conflicts' => array(),
-		'suggests' => array(),
-	),
-	'suggests' => array(),
-);
-
-?>
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Autoswitch to list view',
+    'description' => 'If page module and a sys folder is selected, a redirect to the list module is done.y',
+    'category' => 'backend',
+    'author' => 'Georg Ringer',
+    'author_email' => 'mail@ringer.it',
+    'state' => 'beta',
+    'clearCacheOnLoad' => true,
+    'version' => '2.0.0',
+    'constraints' =>
+        [
+            'depends' => [
+                'typo3' => '7.6.0-8.9.99'
+            ],
+            'conflicts' => [],
+            'suggests' => [],
+        ]
+];
