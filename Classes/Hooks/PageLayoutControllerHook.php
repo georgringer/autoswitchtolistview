5ad64769a9c5e995713e6074636c46d283dc0159
Josef Glatz <j.glatz@supseven.at>
2018-11-21 15:28:36 +0100

    [TASK] Safely omit the type attribute of the <script> tag

--- Classes/Hooks/PageLayoutControllerHook.php
+++ Classes/Hooks/PageLayoutControllerHook.php
@@ -20,15 +20,15 @@ class PageLayoutControllerHook
         $out = '';
 
         // If doktype is type sysfolder
-        if ($parentObject->pageinfo['doktype'] == PageRepository::DOKTYPE_SYSFOLDER) {
+        if ($parentObject->pageinfo['doktype'] === PageRepository::DOKTYPE_SYSFOLDER) {
             $moduleLoader = GeneralUtility::makeInstance(ModuleLoader::class);
             $moduleLoader->load($GLOBALS['TBE_MODULES']);
             $modules = $moduleLoader->modules;
-            if (is_array($modules['web']['sub']['list'])) {
+            if (\is_array($modules['web']['sub']['list'])) {
                 $tsconfig = BackendUtility::getPagesTSconfig($parentObject->id);
 
                 if (!(isset($tsconfig['autoswitchtolistview.']) && isset($tsconfig['autoswitchtolistview.']['disable']) && $tsconfig['autoswitchtolistview.']['disable'] == 1)) {
-                    $out .= '<script type="text/javascript">top.goToModule(\'web_list\',1);</script>';
+                    $out .= '<script>top.goToModule(\'web_list\',1);</script>';
                 }
             }
         }
