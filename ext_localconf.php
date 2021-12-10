70c48f38d4f29f475d4fdf3e900286ace012a501
Georg Ringer <georg.ringer@gmail.com>
2021-12-10 16:27:53 +0100

    [TASK] Simplify ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,4 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
-
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php']['drawFooterHook']['autoswitchtolistview'] =
     \GeorgRinger\Autoswitchlistview\Hooks\PageLayoutControllerHook::class . '->render';
