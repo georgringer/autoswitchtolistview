ef1fa7dafdaf537d79a9c8155e10642736370d9a
Oliver Klee <typo3-coding@oliverklee.de>
2021-12-16 20:13:53 +0100

    [TASK] Add PHP version requirements
    This helps IDEs like PhpStorm determine the language level to use.

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,6 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' =>
         [
             'depends' => [
+                'php' => '5.5.0-8.9.99',
                 'typo3' => '7.6.0-11.5.99'
             ],
             'conflicts' => [],
